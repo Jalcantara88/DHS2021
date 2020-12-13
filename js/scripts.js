15401b928ec492bd586e58770a0516a27923076d
Julio Alcantara <jalcantara88@gmail.com>
2020-12-13 15:35:57 -0800

    finished ytube playlist, shop, footer video

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(function() {
+    window.addEventListener("DOMContentLoaded", event => {
+        const audio = document.querySelector("audio");
+        audio.volume = 0.2;
+        audio.play();
+      });
+
+
+      
+});
